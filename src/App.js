9bfc3b2c72a775c4db026b8b51e36a925497a7f9
Pavel <pasha120787@yandex.ru>
2019-12-09 20:25:00 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
 		<View activePanel={activePanel} popout={popout}>
 			<Home id='home' fetchedUser={fetchedUser} go={go} />
 			<Persik id='persik' go={go} />
-			<MapView id='mapview' />
+			<MapView id='mapview' go={go} />
 		</View>
 	);
 }
