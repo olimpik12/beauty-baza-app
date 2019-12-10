41ba4e45c1c5fec1b0aad9391254b4c630715503
Pavel <pasha120787@yandex.ru>
2019-12-10 22:40:27 +0300

    First commit

--- src/panels/MapView.js
+++ src/panels/MapView.js
@@ -21,7 +21,7 @@ const MapView = props => (
 			Карта
 		</PanelHeader>
 		<YMaps>
-			<Map width=100% height=100% defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
+			<Map className="mapview" width="100vw" height="100vh" defaultState={{ center: [55.75, 37.57], zoom: 9 }} />
     </YMaps>
 	</Panel>
 );
