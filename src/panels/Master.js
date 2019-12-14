823ff14cd3d55487edadbd21209c34c32508b629
Pavel <pasha120787@yandex.ru>
2019-12-14 21:34:41 +0300

    First commit

--- src/panels/Master.js
+++ src/panels/Master.js
@@ -17,7 +17,7 @@ const osName = platform();
 const Master =({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader
-			left={<HeaderButton onClick={() => window.history.back()}>
+			left={<HeaderButton onClick={props.go} data-to="profile">
 				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
 			</HeaderButton>}
 		>
