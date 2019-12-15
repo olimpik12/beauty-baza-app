a2ca382bc51302b1911584e58d823fe201fe5453
Pavel <pasha120787@yandex.ru>
2019-12-16 00:52:46 +0300

    First commit

--- src/panels/Profile.js
+++ src/panels/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { platform, IOS, Select, Textarea, FormLayout } from '@vkontakte/vkui';
+import { platform, IOS, Select, Textarea, FormLayout, Input } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
