747734215bbb805b40e2a95a6a8a2ea54eb3146d
Pavel <pasha120787@yandex.ru>
2019-12-17 21:49:17 +0300

    First commit

--- src/panels/Profile.js
+++ src/panels/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { platform, IOS, Select, Textarea, FormLayout, Input } from '@vkontakte/vkui';
+import { platform, IOS, Select, Textarea, FormLayout, Input, Checkbox } from '@vkontakte/vkui';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
 import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
@@ -39,7 +39,7 @@ const Profile =({ id, go, fetchedUser }) => (
     </Group>}
 	<Group>
    <FormLayout>
-	 <Div top="Какие услуги вы предоставляете?" />
+	 <Div top="Какие услуги вы предоставляете?">
 	 <Checkbox>Маникюр</Checkbox>
 	 <Checkbox>Педикюр</Checkbox>
 	 <Checkbox>Наращивание ресниц</Checkbox>
