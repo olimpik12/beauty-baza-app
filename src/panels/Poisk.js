92dcaa6f48793f1b6c0a45ffac5bfa54cf33b5c5
Pavel <pasha120787@yandex.ru>
2019-12-21 13:08:17 +0300

    First commit

--- src/panels/Poisk.js
+++ src/panels/Poisk.js
@@ -17,7 +17,7 @@ const Spisok = (go, props) => {
 
 	return <Cell
 		photo="https://sun1-86.userapi.com/c845321/v845321561/207aab/XuC_Bty9geU.jpg"
-		description=тест
+		description="тест"
 		bottomContent={<Button onClick={go} data-to="master">Смотреть карточку</Button>}
 		before={<Avatar src="https://sun1-86.userapi.com/c845321/v845321561/207aab/XuC_Bty9geU.jpg" size={80}/>}
 		size="l"
