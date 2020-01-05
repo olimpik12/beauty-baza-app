ba884f05e4e9afd789a306edda59fd3a1849c96f
olimpik12 <58188145+olimpik12@users.noreply.github.com>
2020-01-05 22:30:13 +0300

    Commit
    111

--- src/panels/MapView.js
+++ src/panels/MapView.js
@@ -1,39 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { platform, IOS, Search } from '@vkontakte/vkui';
-import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
-import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
-import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
-import Icon24Back from '@vkontakte/icons/dist/24/back';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
-import PanelPoisk from './PanelPoisk';
-import '../style/style.css'
+import React from "react";
+import PropTypes from "prop-types";
+import { platform, IOS } from "@vkontakte/vkui";
+import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
+import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
+import HeaderButton from "@vkontakte/vkui/dist/components/HeaderButton/HeaderButton";
+import { YMaps, Map } from "react-yandex-maps";
+import Icon28ChevronBack from "@vkontakte/icons/dist/28/chevron_back";
+import Icon24Back from "@vkontakte/icons/dist/24/back";
+import PanelPoisk from "./PanelPoisk";
+import "../style/style.css";
 
 const osName = platform();
 
 const MapView = props => (
-	<Panel id={props.id}>
-	<PanelHeader
-		left={<HeaderButton onClick={props.go} data-to="home">
-			{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
-		</HeaderButton>}
-		>
-			Карта
-		</PanelHeader>
+  <Panel id={props.id}>
+    <PanelHeader
+      left={
+        <HeaderButton onClick={props.go} data-to="home">
+          {osName === IOS ? <Icon28ChevronBack /> : <Icon24Back />}
+        </HeaderButton>
+      }
+    >
+      Карта
+    </PanelHeader>
 
-		<PanelPoisk/>
-		
-		<YMaps>
-			<Map className="mapview" width="100vw" height="100vh" defaultState={{ center: [55.75, 37.57], zoom: 15 }} />
+    <PanelPoisk />
+
+    <YMaps>
+      <Map
+        className="mapview"
+        width="100vw"
+        height="100vh"
+        defaultState={{ center: [55.75, 37.57], zoom: 15 }}
+      />
     </YMaps>
-	</Panel>
+  </Panel>
 );
 
 MapView.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired
 };
 
 export default MapView;
--- src/panels/PanelPoisk.js
+++ src/panels/PanelPoisk.js
@@ -1,36 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { platform, IOS, Select, FormLayout, Search, Input, Cell } from '@vkontakte/vkui';
-import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
-import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton';
-import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
-import Icon24Back from '@vkontakte/icons/dist/24/back';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import React from "react";
+import PropTypes from "prop-types";
+import { Select, Input, Cell } from "@vkontakte/vkui";
 
-
-const osName = platform();
-
-
-const PanelPoisk = props => (
-	<div>
-	<Cell>
-	  <Select placeholder="Выберите услугу">
-  <option value="m">маникюр</option>
-  <option value="f">педикюр</option>
-    </Select>
-	</Cell>
-	<Cell>
-	<Input value="город" />
-	</Cell>
-	</div>
+const PanelPoisk = () => (
+  <div>
+    <Cell>
+      <Select placeholder="Выберите услугу">
+        <option value="m">маникюр</option>
+        <option value="f">педикюр</option>
+      </Select>
+    </Cell>
+    <Cell>
+      <Input value="город" />
+    </Cell>
+  </div>
 );
 
 PanelPoisk.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  go: PropTypes.func.isRequired
 };
-
 
 export default PanelPoisk;
